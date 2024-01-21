3fed4bbecfa5fe6762673816de76d36c64e59429
UnLeks <36797639+UnLeks@users.noreply.github.com>
2024-01-21 16:14:46 +0700

    Add files via upload

---
+++ PolarCodingStand/PolarEncoder/PolarEncoder.cs
@@ -0,0 +1,56 @@
+﻿namespace PolarEncoderDLL
+{
+    public static class PolarEncoder
+    {
+        public static (int[] codedMessage, int[] precodedMessage) PolarMessageEncoding(int messageLength, int[] sentInfoWord, int[] frozenBitsIndices, int[] infoBitsIndices, int[,] G)
+        {
+            int[] frozenBitsValues = new int[messageLength / 2]; // задание массива значений замороженных бит
+            int[] precodedMessage = new int[messageLength]; // задание промежуточного закодированного слова
+
+            for (int k = 0; k < messageLength / 2; k++)
+            {
+                precodedMessage[frozenBitsIndices[k]] = frozenBitsValues[k]; // расстановка замороженных бит
+            }
+
+            for (int k = 0; k < messageLength / 2; k++)
+            {
+                precodedMessage[infoBitsIndices[k]] = sentInfoWord[k]; // расстановка информационных бит
+            }
+
+            int[] codedMessage = Mod(Multiply(precodedMessage, G));
+
+            return (codedMessage, precodedMessage);
+        }
+
+        public static int[] Multiply(int[] vector, int[,] matrix)
+        {
+            int[] result = new int[vector.Length];
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < vector.Length; j++)
+                {
+                    sum += vector[j] * matrix[j, i];
+                }
+
+                result[i] = sum;
+            }
+
+            return result;
+        }
+
+        public static int[] Mod(int[] vector)
+        {
+            int[] result = new int[vector.Length];
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                result[i] = vector[i] % 2;
+            }
+
+            return result;
+        }
+    }
+}
